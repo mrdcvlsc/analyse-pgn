8f28687878c7789b74387b847b3c74c2b1954ec1
Mordecai Velasco <59404502+mrdcvlsc@users.noreply.github.com>
2021-10-05 09:01:28 +0800

    Update board.hpp

--- board.hpp
+++ board.hpp
@@ -1,1 +1,13 @@
 
+// board state class
+
+// : read_pgn_constructor()
+// : next_move()
+// : prev_move()
+// : display()
+
+// : game_info [vec|pair]
+// : uci_moves [vec]
+// : piece_dict
+// : - counts
+// : - positions
