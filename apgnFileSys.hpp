4a829b936c071532392da15ce131082e1a0a014d
mrdcvlsc <mrdcvlsc@gmail.com>
2025-10-27 12:49:55 +0800

    refactor: move function implementation from header files to source files
    fix: update outdated executable locations when executing child processes in ubuntu/linux

--- apgnFileSys.hpp
+++ apgnFileSys.hpp
@@ -18,46 +18,8 @@
 
 namespace apgnFileSys
 {
-    void deleteFile(const std::string& filename)
-    {
-        try {
-            if (std::filesystem::remove(filename))
-                std::cout << "...\n";
-            else
-                std::cout << "file " << filename << " not found.\n";
-        }
-        catch(const std::filesystem::filesystem_error& err)
-        {
-            throw std::system_error(err);
-        }
-    }
-
-    std::string getExecpath()
-    {
-        #if defined(__linux__)
-        char result[PATH_MAX];
-        ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
-        const char *path;
-        if (count != -1) {
-            path = dirname(result);
-        }
-        return std::filesystem::path(path).string();
-        #elif defined(_WIN32)
-        WCHAR path[MAX_PATH];
-        GetModuleFileNameW(NULL, path, MAX_PATH);
-
-        std::wstring string_to_convert = path;
-
-        //setup converter
-        using convert_type = std::codecvt_utf8<wchar_t>;
-        std::wstring_convert<convert_type, wchar_t> converter;
-
-        //use converter (.to_bytes: wstr->str, .from_bytes: str->wstr)
-        std::string converted_str = converter.to_bytes( string_to_convert );
-        
-        return std::filesystem::path(converted_str.substr(0,converted_str.size()-9)).string();
-        #endif
-    }
+    void deleteFile(const std::string& filename);
+    std::string getExecpath();
 }
 
 #endif
---
+++ winProcRun.cpp
@@ -0,0 +1,1 @@
+#include "winProcRun.hpp"
