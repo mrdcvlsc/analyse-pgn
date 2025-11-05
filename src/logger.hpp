1365ad84065a795868482f5f78c670228b75eb8a
mrdcvlsc <mrdcvlsc@gmail.com>
2025-11-05 16:25:28 +0800

    update logger

--- src/logger.hpp
+++ src/logger.hpp
@@ -15,8 +15,8 @@ std::string get_timestamp();
     #define DEBUG_LOG_HEX(x) std::cout << get_timestamp() << "|t(" << std::this_thread::get_id() << ")" << "[" << std::filesystem::path(std::string(__FILE__)).filename().string()  << ':' << __LINE__ << "]: " << std::hex << (x) << std::dec << '\n';
     #define DEBUG_ERR_HEX(x) std::cerr << get_timestamp() << "|t(" << std::this_thread::get_id() << ")" << "[" << std::filesystem::path(std::string(__FILE__)).filename().string()  << ':' << __LINE__ << "]: " << std::hex << (x) << std::dec << '\n';
 #else
-    #define DEBUG_LOG(x) // noop
-    #define DEBUG_ERR(x) // noop
-    #define DEBUG_LOG_HEX(x) // noop
-    #define DEBUG_ERR_HEX(x) // noop
+    #define DEBUG_LOG(x) do { } while (0)
+    #define DEBUG_ERR(x) do { } while (0)
+    #define DEBUG_LOG_HEX(x) do { } while (0)
+    #define DEBUG_ERR_HEX(x) do { } while (0)
 #endif
